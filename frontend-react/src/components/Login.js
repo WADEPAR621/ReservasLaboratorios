0e8f378a60992801e8ca82739faae73f42a0a994
Thetander <joelzunta@hotmail.com>
2023-12-08 11:13:09 -0500

    actualizacion de pacquetes, Creacion de login.js(Creacion de Estructura) y styles.js

---
+++ frontend-react/src/components/Login.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const Login = () => {
+
+
+
+
+return(
+<>
+
+<div>
+    <h1>Hola Mundo</h1>
+    <h1>aaaaa</h1>
+
+</div>
+
+
+</>
+);    
+} 
+export default Login;
