9bf885ef2e5d646df15de674c18a1f02a71d9414
Kenneth <kparedes9203@uta.edu.ec>
2023-12-29 17:13:11 -0500

    Configuracion de las rutas en el Front y arreglar la consulta de la base de datos para poder arreglar el combo box. Ahora se puede cambiar la imagen y la descripcion segun el piso

---
+++ BackEnd-Rails/app/views/administrators/_administrator.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! administrator, :id, :NOM_ADM, :APE_ADM, :COR_USE, :SEC_USE, :created_at, :updated_at
+json.url administrator_url(administrator, format: :json)
--- frontend-vite/src/components/MapaPisos.jsx
+++ frontend-vite/src/components/MapaPisos.jsx
@@ -3,20 +3,23 @@ import { Link } from "react-router-dom"
 
 import '../styles/MapaPisos.css';
 import NavBar from "./NavbarMenu";
+
+
 const MapaPisos = () => {
-    
+
 
     useEffect(() => {
-        fetchPisos();
+        fetchPisos(1);
     }, []);
 
     //Metodo para obtener todas las reservas dependiendo del usuario
 
-    const [pisos, setPisos]= useState([])
-    const fetchPisos = async () => {
+    const [pisos, setPisos] = useState([])
+    const fetchPisos = async (id) => {
+        
         try {
             console.log(pisos);
-            const response = await fetch('http://localhost:3000/floors', {
+            const response = await fetch(`http://localhost:3000/floors/${id}`, {
                 method: "GET"
             });
             if (!response.ok) {
@@ -33,9 +36,12 @@ const MapaPisos = () => {
 
     // Manejador de eventos para cambiar la selección
     const [selectedOption, setSelectedOption] = useState('');
-    
+
     const handleSelectChange = (event) => {
         setSelectedOption(event.target.value);
+        console.log(event.target.value);
+        const id = event.target.value;
+        fetchPisos(id);
     };
 
     //Comienzo del HTML en React
@@ -46,24 +52,20 @@ const MapaPisos = () => {
                 <div className="container_info">
                     <h3>PISO:</h3>
                     <select id="comboBox" value={selectedOption} onChange={handleSelectChange}>
-                        <option value="PISO1">PISO 1:</option>
-                        <option value="PISO2">PISO 2:</option>
-                        <option value="PISO3">PISO 3:</option>
+                        <option value="1">PISO 1:</option>
+                        <option value="2">PISO 2:</option>
+                        <option value="3">PISO 3:</option>
                     </select>
                     <h3>DESCRIPCION:</h3>
-                    <p>Lcilis awduabwduabwab!</p>
+                    <p>{pisos?.DES_PIS}</p>
                     <h3>MAS INFORMACION:</h3>
                     <div className="Habitaciones">
-                        {pisos.map((pisos) => {
-                            return(
-                                <p>{pisos.DES_PIS}</p>
-                            )
-                        })}
+                        <p>{pisos?.DES_PIS}</p>
                     </div>
                 </div>
                 <div className="container_image">
                     <img className="Plano_imagen"
-                        src="https://i.ibb.co/bv0RVMb/Captura-de-pantalla-2023-11-22-230109.png"></img>
+                        src={pisos?.IMG_PIS}></img>
                 </div>
             </div>
         </div>
