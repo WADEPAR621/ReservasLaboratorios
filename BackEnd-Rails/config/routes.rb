4b97cc9a655f94166e59e2ba655f2cd0150c1af9
WADEPAR621 <raulparedes20031221@gmail.com>
2023-12-29 20:30:54 -0500

    Arreglo de la consulta de la base de datos

--- BackEnd-Rails/config/routes.rb
+++ BackEnd-Rails/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   resources :books, only: [:index,:show,:destroy]
-  resources :rooms, only: [:index,:show,:destroy]
+  resources :rooms, only: [:index,:show,:destroy, :new] do
+    collection do
+      get 'rooms/by_floor/:floor_id', to: 'rooms#by_floor', as: 'rooms_by_floor'
+    end
+  end
   resources :floors, only: [:index,:show,:destroy]
   resources :students, only: [:index,:show,:destroy]
   resources :tecnicos, only: [:index,:show,:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-
 end
