c719833f1d6167a608c6a3040ae0555748122bc7
Thetander <joelzunta@hotmail.com>
2023-12-30 13:51:45 -0500

    terminado de estilo de los campos

--- frontend-vite/src/components/Sing_up.tsx
+++ frontend-vite/src/components/Sing_up.tsx
@@ -63,42 +63,23 @@ const SignUp: React.FC = () => {
                     <label className="inputbox__label" htmlFor="">Nombre</label>
                 </div>
                 <div className="inputbox">
-                    <input className="inputbox__input" type="number" required value={newCuenta.ncedula} onChange={(e) => setNewCuenta({ ...newCuenta, ncedula: e.target.value })}/>
+                    <input className="inputbox__input" type="text" required value={newCuenta.ncedula} onChange={(e) => setNewCuenta({ ...newCuenta, ncedula: e.target.value })} />
                     <label className="inputbox__label" htmlFor="">Numero de celular</label>
                 </div>
                 <div className="inputbox">
-                    <input
-                        type="date"
-                        required
-                        value={newCuenta.fechaNacimiento}
-                        onChange={(e) => setNewCuenta({ ...newCuenta, fechaNacimiento: e.target.value })}
-                    />
+                    <input className="inputbox__input" type="date" required value={newCuenta.fechaNacimiento} onChange={(e) => setNewCuenta({ ...newCuenta, fechaNacimiento: e.target.value })} />
                     <label className="inputbox__label" htmlFor="inputbox__Date">Dia de Nacimiento</label>
                 </div>
                 <div className="inputbox">
-                    <input
-                        type="text"
-                        required
-                        value={newCuenta.correoElectronico}
-                        onChange={(e) => setNewCuenta({ ...newCuenta, correoElectronico: e.target.value })}
-                    />
+                    <input className="inputbox__input" type="text" required value={newCuenta.correoElectronico} onChange={(e) => setNewCuenta({ ...newCuenta, correoElectronico: e.target.value })}/>
                     <label className="inputbox__label" htmlFor="">Correo Electronico</label>
                 </div>
                 <div className="inputbox">
-                    <input
-                        type="password"
-                        required
-                        value={newCuenta.contrasena}
-                        onChange={(e) => setNewCuenta({ ...newCuenta, contrasena: e.target.value })}
-                    />
+                    <input className="inputbox__input" type="password" required value={newCuenta.contrasena} onChange={(e) => setNewCuenta({ ...newCuenta, contrasena: e.target.value })}/>
                     <label className="inputbox__label" htmlFor="">Contraseña</label>
-                </div>className = "inputbox__label"
+                </div>
                 <div className="inputbox">
-                    <input
-                        type="password"
-                        required
-
-                    />
+                    <input className="inputbox__input"type="password"required/>
                     <label className="inputbox__label" htmlFor="">Confirmar Contraseña</label>
                 </div>
                 <br />
