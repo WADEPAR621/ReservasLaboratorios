ff3e286dcc16b6d4bf82c192c107a22bccc573f5
Joshua Herrera <sebas.herrera3152@gmail.com>
2024-01-06 19:31:24 -0500

    Modificacion App.jsx

--- frontend-vite/src/components/pages/AdminCRUD.jsx
+++ frontend-vite/src/components/pages/AdminCRUD.jsx
@@ -1,72 +1,89 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/pages/Login';
+import SignUp from './components/pages/Sing_up';
+import SobreNosotros from './components/pages/SobreNosotros';
+import AdminRoutes from './AdminRoutes';
+import { useState, useEffect } from 'react';
+import StudentRoutes from './StudentRoutes';
 
-const AdminCRUD = () => {
-  return (
-    <div>
-      <h3>Bienvenido Administrador</h3>
-      <h4>¿Qué acción deseas realizar?</h4>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Agregar Estudiante</h5>
-                <p className="card-text">
-                  Agrega un estudiante a la base de datos.
-                </p>
-                <a href="#" className="btn btn-danger">
-                  Agregar
-                </a>
-              </div>
-            </div>
-          </div>
+const ProtectedRoute = ({ element, ...props }) => {
+  //CONSTANTES DE PARAMETROS
+  const [handlePiso, setEditPiso] = useState([]);
 
-          <div className="col-lg-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Añadir laboratorio</h5>
-                <p className="card-text">
-                  Agrega un estudiante a la base de datos.
-                </p>
-                <a href="#" className="btn btn-danger">
-                  Agregar
-                </a>
-              </div>
-            </div>
-          </div>
+  const { isAuthenticated } = useAuth();
 
-          <div className="col-lg-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Agregar Horarios</h5>
-                <p className="card-text">
-                  Agrega o elimina horarios de la base de datos.
-                </p>
-                <a href="#" className="btn btn-danger">
-                  Agregar
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Administrar Peticiones</h5>
-                <p className="card-text">
-                  Aprueba o Deniega las peticiones de los estudiantes.
-                </p>
-                <a href="#" className="btn btn-danger">
-                  Agregar
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  return isAuthenticated ? (
+    element
+  ) : (
+    <Navigate to="/" replace state={{ from: props.location }} />
   );
 };
 
-export default AdminCRUD;
+const initState = {
+  usuario: "",
+  contrasena: "",
+  rol: "",
+  isAuth: false
+}
+
+const App = () => {
+
+  const [usuarioLo, setUsuario] = useState(sessionStorage.getItem("Login") || initState);
+  useEffect(() => {
+    if (usuarioLo.isAuth && !sessionStorage.getItem("Login")) {
+      sessionStorage.setItem("Login", usuarioLo);
+    }
+    console.log(usuarioLo);
+  }, [usuarioLo]);
+
+  return (
+    <Router>
+      <Routes>
+        {usuarioLo.isAuth ? (
+          usuarioLo.rol == "admin" ?(
+              <Route
+                path="/*"
+                element={<AdminRoutes />}
+              />
+            )
+            :
+            (
+              <Route
+                path="/*"
+                element={<StudentRoutes />}
+              />
+            )
+        ) : (
+          <>
+            <Route
+              path={`/SobreNosotros`}
+              element={<SobreNosotros />}
+            />
+            <Route
+              path={`/Sing_up`}
+              element={<SignUp />}
+            />
+            
+
+            <Route
+              path={`/Login`}
+              element={<Login usuarioLo={usuarioLo} setUsuario={setUsuario} />}
+            />
+
+            <Route
+              path={`/*`}
+              element={<Navigate to={"/Login"} />}
+            />
+          </>
+
+          
+
+          
+        )}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
