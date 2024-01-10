73732d719acf721b5070a7fb279b64d872aa662f
Thetander <joelzunta@hotmail.com>
2024-01-09 23:38:18 -0500

    Se modifico el encabezado

--- frontend-vite/src/components/Navbar.jsx
+++ frontend-vite/src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
+import '../styles/navbar.css'
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg ">
             <div className="container-fluid">
                 <Link to='/'>
-                    <img src={logo} width='50' alt="Logo" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/93/Escudo_de_la_Universidad_T%C3%A9cnica_de_Ambato.png" width='75%'></img>
                 </Link>
+                <div className="navbar_text">
+                <h1>UTA</h1>
+                <h2>UNIVERSIDAD TÉCNICA DE AMBATO</h2>
+            </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -18,7 +23,7 @@ const Navbar = () => {
                             <Link className="nav-link" to='https://servicios.uta.edu.ec/SistemaIntegrado/Cuenta/Login'>Sistema Integrado</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to='/Login'>Inicia Sesion</Link>
+                            <Link className="nav-link" to='/Login'>Inicia Sesión</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to='https://www.uta.edu.ec/v4.0/?page=4'>UTA Oficial</Link>
